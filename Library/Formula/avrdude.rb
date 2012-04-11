872b0cd3e0991d1a26229be57bd1de6ef037ad1c
Penn Su <pennsuaccount@gmail.com>
2012-04-11 15:50:26 +0800

    Update Library/Formula/avrdude.rb

--- Library/Formula/avrdude.rb
+++ Library/Formula/avrdude.rb
@@ -5,13 +5,17 @@ class Avrdude < Formula
   homepage 'http://savannah.nongnu.org/projects/avrdude/'
   md5 '3a43e288cb32916703b6945e3f260df9'
 
+  head 'http://svn.savannah.nongnu.org/svn/avrdude/trunk/avrdude'
+
   depends_on 'libusb-compat' if ARGV.include? '--with-usb'
+  depends_on 'automake' unless File.exists? "/Developer"
 
   def options
     [['--with-usb', 'Compile AVRDUDE with USB support.']]
   end
 
   def install
+    system "./bootstrap" if ARGV.build_head?
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
     system "make install"
