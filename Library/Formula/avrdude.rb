1039a5ab30d408d2c09ff06631efb5b25df982c8
Penn Su <pennsuaccount@gmail.com>
2012-04-11 15:53:23 +0800

    Update Library/Formula/avrdude.rb

--- Library/Formula/avrdude.rb
+++ Library/Formula/avrdude.rb
@@ -2,10 +2,9 @@ require 'formula'
 
 class Avrdude < Formula
   url 'http://download.savannah.gnu.org/releases/avrdude/avrdude-5.11.1.tar.gz'
-  homepage 'http://savannah.nongnu.org/projects/avrdude/'
   md5 '3a43e288cb32916703b6945e3f260df9'
-
   head 'http://svn.savannah.nongnu.org/svn/avrdude/trunk/avrdude'
+  homepage 'http://savannah.nongnu.org/projects/avrdude/'
 
   depends_on 'libusb-compat' if ARGV.include? '--with-usb'
   depends_on 'automake' unless File.exists? "/Developer"
